5ec5954d75b998c9eaf2ab42a3c860b1cca62e0f
Nic Flenghi <nic.flenghi@gmail.com>
2022-08-25 06:17:10 +0000

    ex00 done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,25 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fnichola <fnichola@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/24 12:51:37 by fnichola          #+#    #+#             */
-/*   Updated: 2022/08/24 12:59:23 by fnichola         ###   ########.fr       */
+/*   Created: 2022/08/25 06:14:39 by fnichola          #+#    #+#             */
+/*   Updated: 2022/08/25 06:15:03 by fnichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <limits>
 #include <string>
-#include <algorithm>
 
 int main(int argc, char **argv)
 {
 	(void)argc;
 
-	std::string str(argv[1]);
-	std::transform(str.begin(), str.end(), str.begin(), [] (unsigned char c) { return std::toupper(c); });
-	std::cout << str << std::endl;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; i < argc; i++)
+	{
+		std::string str(argv[i]);
+		for (size_t j = 0; j < str.length(); j++)
+			std::cout << (unsigned char) std::toupper(str[j]);
+	}
+	std::cout << std::endl;
 }
